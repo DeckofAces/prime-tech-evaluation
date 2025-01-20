de2f668fe2ca2e90300bac754b9761364e6bb455
DeckofAces <52782439+DeckofAces@users.noreply.github.com>
2025-01-20 10:37:50 +0100

    * install bootstrap * create nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgbNavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
